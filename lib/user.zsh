0bd5ab02e0cf445a00e18b28f52653cfc2488f21
distroy <distroy@163.com>
2014-06-26 16:37:56 +0800

    comment man color

--- lib/user.zsh
+++ lib/user.zsh
@@ -9,13 +9,13 @@ export LANGUAGE=$LANG
 export GREP_COLOR='1;32'
 export GREP_OPTIONS='--color=auto'
 
-export LESS_TERMCAP_mb=$'\E[01;31m'
-export LESS_TERMCAP_md=$'\E[01;31m'
-export LESS_TERMCAP_me=$'\E[0m'
-export LESS_TERMCAP_se=$'\E[0m'
-export LESS_TERMCAP_so=$'\E[01;44;33m'
-export LESS_TERMCAP_ue=$'\E[0m'
-export LESS_TERMCAP_us=$'\E[01;32m'
+export LESS_TERMCAP_mb=$'\E[01;31m'         # begin blinking
+export LESS_TERMCAP_md=$'\E[01;31m'         # begin bold
+export LESS_TERMCAP_me=$'\E[0m'             # end mode
+export LESS_TERMCAP_se=$'\E[0m'             # end standout-mode
+export LESS_TERMCAP_so=$'\E[01;44;33m'      # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\E[0m'             # end underline
+export LESS_TERMCAP_us=$'\E[01;32m'         # begin underline
 
 export SVN_EDITOR=vim
 
