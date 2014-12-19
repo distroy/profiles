05bd1eb8848a4724b5dc72658ecfc5b530c0b338
distroytan <distroytan@tencent.com>
2014-12-19 10:29:22 +0800

    zshrc: bug-fixed GREP_OPTIONS

--- lib/sh/user.zsh
+++ lib/sh/user.zsh
@@ -10,7 +10,7 @@ export LANG=zh_CN.utf-8
 export LANGUAGE=$LANG
 
 export GREP_COLOR='1;32'
-export GREP_OPTIONS="grep --color=auto --exclude-dir=.git --exclude-dir=.svn --exclude-dir=.hg"
+export GREP_OPTIONS="--color=auto --exclude-dir=.git --exclude-dir=.svn --exclude-dir=.hg"
 
 export LESS_TERMCAP_mb=$'\E[01;31m'         # begin blinking
 export LESS_TERMCAP_md=$'\E[01;31m'         # begin bold
