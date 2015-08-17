f937d3831d3514e635c5edca832406e6c3f56e8c
distroytan <distroytan@tencent.com>
2015-08-17 22:21:58 +0800

    bash: updated theme

--- bash/lib/theme.bash
+++ bash/lib/theme.bash
@@ -3,5 +3,5 @@
 #
 
 
-export PS1='\[\033[01;32m\]\u@$(hostname -I) \[\033[01;33m\]\w\[\033[01;35m\]$(type __git_ps1 &>/dev/null && __git_ps1 " (%s)")
+export PS1='\[\033[01;32m\]\u@$(hostname -I 2>/dev/null || echo "\h") \[\033[01;33m\]\w\[\033[01;35m\]$(type __git_ps1 &>/dev/null && __git_ps1 " (%s)")
 \[\033[01;37m\]\$ \[\033[00m\]'
