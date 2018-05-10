6814d10b265b43d351acf2cd64416a71589f5d84
distroytan <distroytan@pacewear.cn>
2018-05-10 15:34:00 +0800

    package: ycm install: added

---
+++ package/ycm.install.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+#
+# Copyright (C) distroy
+#
+
+
+./install.py \
+    --system-libclang \
+    --enable-coverage \
+    --js-completer \
+    --tern-completer \
+    --clang-completer
