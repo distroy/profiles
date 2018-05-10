cf67f75839fa8d6802b611f9a69f433309d4ab5b
distroy <distroy@163.com>
2018-05-10 21:12:02 +0800

    package: vim configure: set the dir of python and python3

--- package/vim.configure.sh
+++ package/vim.configure.sh
@@ -11,6 +11,8 @@
     --enable-luainterp=yes \
     --enable-rubyinterp=yes \
     --enable-pythoninterp=yes \
+    --with-python-config-dir=/usr/lib64/python2.7/config \
     --enable-python3interp=yes \
+    --with-python3-config-dir=/usr/lib64/python3.5/config \
     --enable-multibyte \
     --enable-cscope
