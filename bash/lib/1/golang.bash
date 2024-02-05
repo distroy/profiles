1bd5f58ed87752155628eda333d790c3a7c837a8
distroy <distroy@163.com>
2024-02-05 11:01:22 +0800

    feat(zsh & bash: lib golang): add go-test-loop for check the directory failed to go test

--- bash/lib/1/golang.bash
+++ bash/lib/1/golang.bash
@@ -24,5 +24,43 @@ function __ld_golang_version_switch() (
         ln -sv "$1" "go"
     fi
 )
-
 alias go-switch=__ld_golang_version_switch
+
+# export GOPROXY=https://goproxy.io,direct
+# export GOPRIVATE=
+export GOPROXY=https://goproxy.cn,direct
+
+alias go-test='go test -gcflags="all=-l"'
+
+function __ld_golang_test_loop_run() (
+    local dir="$1"
+    local code=0
+    local line
+    local r
+    local tmp
+    if [[ "$dir" == "" ]]; then
+        dir="."
+    elif [[ "$dir" != "." && "$dir" != ".*" && "$dir" != "/*" ]]; then
+        dir="./$dir"
+    fi
+    ld_msgg "$dir"
+    find "$dir" -type d | grep -E -v '\/(vendor|\..*)(\/|$)' | while read line; do
+        ld_msgg "=== RUN directory: $line"
+        tmp=$(find "$line" -name '*.go' -maxdepth 1 | wc -l)
+        if (( tmp == 0 )); then
+            ld_msgy "=== SKIP directory: $line [no Go files]"
+            continue
+        fi
+
+        go-test -v "$line"
+        r="$?"
+        if (( r == 0 )); then
+            ld_msgg "=== PASS directory: $line"
+        else
+            code="$r"
+            ld_msgr "=== FAIL directory: $line [exit code $r]"
+        fi
+    done
+    exit $code
+)
+alias go-test-loop=__ld_golang_test_loop_run
